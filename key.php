ec1c751dab735dc39f6977fe7108cd1e1abd20cd
fbprageeth <shanarusha_Sudarushanaru@epark.co.jp>
2017-10-24 13:18:01 +0900

    Create key.php

---
+++ key.php
@@ -0,0 +1,11 @@
+<?php
+
+    /*
+        Reason  : 個人情報暗号化（キー設定）
+        Creator : Prageeth Sudarshana
+        Date    : 17/09/29
+     */
+
+return array(
+        'encry'                 => 'jgX69ghjeBNBl01FT609Xvr46hKUD5jg8jppPo0O', //暗号キー
+);
